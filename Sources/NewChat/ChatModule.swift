7636d7bba967390ebd2f427245d2e24d477bf00c
Sanjay Kumar <sanjay.khatri02@outlook.com>
2024-06-25 07:47:13 +0500

    Code CreateChat

--- Sources/NewChat/ChatModule.swift
+++ Sources/NewChat/ChatModule.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+public func createChatViewController() -> ChatViewController? {
+    print("Test...")
+    let bundle = Bundle.module
+    let storyboard = UIStoryboard(name: "Chat", bundle: bundle)
+    let viewController = storyboard.instantiateViewController(withIdentifier: "ChatViewController") as? ChatViewController
+    return viewController
+    
+}
