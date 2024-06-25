205a7f2fbe065858b2dc844ff27aca08950779c7
Sanjay Kumar <sanjay.khatri02@outlook.com>
2024-06-25 07:17:58 +0500

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,77 @@ import PackageDescription
 
 let package = Package(
     name: "NewChat",
+    platforms: [
+            .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NewChat",
             targets: ["NewChat"]),
     ],
+    
+    dependencies: [
+            // Add your dependencies here
+            .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.0"),
+            .package(url: "https://github.com/andreybuksha/LMCSideMenu.git", from: "0.1.6"),
+            .package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "13.5.0"),
+            .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+            .package(url: "https://github.com/SVProgressHUD/SVProgressHUD.git", from: "2.2.5"),
+            .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", from: "5.0.0"),
+            .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", from: "6.5.6"),
+            .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"), // Firebase SDK
+            .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+            .package(url: "https://github.com/cesarferreira/SwiftEventBus.git", from: "5.1.0"),
+            //.package(url: "https://github.com/bahlo/SwiftGif.git", from: "1.7.0"),
+            .package(url: "https://github.com/ccgus/fmdb.git", from: "2.7.5"),
+            .package(url: "https://github.com/moozzyk/SignalR-Client-Swift", from: "0.9.0"),
+//            .package(url: "https://github.com/devxoul/Toaster.git", from: "2.3.0"),
+//            .package(url: "https://github.com/hirohisa/ImageLoaderSwift", from: "0.6.0"),
+            .package(url: "https://github.com/mikaoj/BSImagePicker.git", from: "3.3.0"),
+            .package(url: "https://github.com/evgenyneu/Cosmos.git", from: "23.0.0")
+
+            ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NewChat"),
+            name: "NewChat",
+            dependencies: [
+                            "Alamofire",
+//                            "LMCSideMenu",
+//                            "ProgressHUD",
+                            "SwiftyJSON",
+                            "SVProgressHUD",
+//                            "NVActivityIndicatorView",
+//                            "IQKeyboardManager",
+//                            .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+//                            .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                            "Kingfisher",
+                            "SwiftEventBus",
+//                            //"SwiftGif",
+//                            "FMDB",
+                            .product(name: "FMDB", package: "fmdb"),
+                            .product(name: "SignalRClient", package: "SignalR-Client-Swift"),
+
+//                            "SignalRClient"
+//                            //"Toaster",
+//                            //"ImageLoader",
+                            "BSImagePicker",
+                            "Cosmos"
+                        ],
+            path: "Sources/NewChat",
+            resources: [
+                            .process("Resources/Storyboards"),
+                            .process("Resources/Xib"),
+                            .process("Resources/Assets.xcassets"),
+                            .process("Resources/Fonts"),
+                            .process("Resources/Images"),
+                            .process("Resources/**/*.gif")
+                        ]
+            
+        ),
+        
         .testTarget(
             name: "NewChatTests",
             dependencies: ["NewChat"]),
